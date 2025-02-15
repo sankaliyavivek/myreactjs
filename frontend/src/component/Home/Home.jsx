737ba0be371c060f70c9f69dc8138d0278e85691
vivek <sankaliyavivek9797@gmail.com>
2025-02-15 15:48:43 +0530

    ok changes

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -12,7 +12,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "https://myreactjsproject-backend.onrender.com";
 const socket = io(`${SOCKET_URL}`, {
     withCredentials: true,
-    transports: ["websocket", "polling"],
+    transports: [ "polling"],
 });
 
 
