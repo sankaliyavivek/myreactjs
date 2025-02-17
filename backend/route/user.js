9c5b5971fbc1d1b6bb7aaada31dad7d573b595e6
vivek <sankaliyavivek9797@gmail.com>
2025-02-17 11:11:52 +0530

    ok

--- backend/route/user.js
+++ backend/route/user.js
@@ -46,9 +46,9 @@
     
             res.cookie("token", token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'strict',
-                maxAge: 24 * 60 * 60 * 1000
+                secure: true,  // Ensure cookies are sent over HTTPS
+                sameSite: 'none',  // Allows cross-site requests
+                maxAge: 24 * 60 * 60 * 1000 
             });
     
             res.json({
