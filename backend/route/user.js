b727fea7c1734189fea4deee82443887abcc0e84
vivek <sankaliyavivek9797@gmail.com>
2025-02-19 16:46:32 +0530

    ok

--- backend/route/user.js
+++ backend/route/user.js
@@ -65,18 +65,18 @@
 
     router.post('/logout', (req, res) => {
         try {
-            res.clearCookie("token", {
+            res.cookie("token", "", {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === "production", // Use HTTPS only in production
-                sameSite: "None", // Required for cross-origin requests
-                path: "/",
-                domain: "https://clientproject-vivek.netlify.app" // Ensure this matches your frontend domain
+                sameSite: 'none',
+                expires: new Date(0), // Expire immediately
+                path: '/' 
             });
-    
-            res.status(200).json({ message: "User logged out successfully" });
+            
+            res.status(200).json({ message: 'User logged out successfully' });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: "Internal server error" });
+            res.status(500).json({ message: 'Internal server error' });
         }
     });
     
