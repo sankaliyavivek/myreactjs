92d9ea5d092306618cf4874036871716c16e5cae
vivek <sankaliyavivek9797@gmail.com>
2025-02-25 16:46:23 +0530

    ok

--- backend/connect.js
+++ backend/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-// sankaliyavivek9797:XVfSG9Y7orBBJu0A
-// mongoose.connect('mongodb+srv://sankaliyavivek9797:XVfSG9Y7orBBJu0A@cluster2.nhkws.mongodb.net/AdminUser')
-//     .then(() => console.log('Connected!'));
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{console.log('Connected!')})
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -4,4 +4,5 @@ const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 export const socket = io(SOCKET_URL, { transports: ["websocket"],withCredentials: true,
     reconnection: true,
     reconnectionAttempts: 5,
-    reconnectionDelay: 1000, });
+    reconnectionDelay: 1000,});
+ 
