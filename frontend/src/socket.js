832608b0496f81602888a3fe25a1ba59fa0a13de
vivek <sankaliyavivek9797@gmail.com>
2025-02-25 12:51:20 +0530

    ok

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
-export const socket = io(SOCKET_URL, { transports: ["websocket"] });
+export const socket = io(SOCKET_URL, { transports: ["websocket"],withCredentials: true,
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000, });
